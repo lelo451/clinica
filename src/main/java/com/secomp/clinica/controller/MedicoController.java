3461c6dd35992d277ed36059a92b08cb38e1e5ad
Vinicius <vinicamarggo@hotmail.com>
2017-09-24 01:57:31 -0300

    medico v1

---
+++ src/main/java/com/secomp/clinica/controller/MedicoController.java
@@ -0,0 +1,59 @@
+package com.secomp.clinica.controller;
+
+import com.secomp.clinica.model.Consulta;
+import com.secomp.clinica.model.Usuario;
+import com.secomp.clinica.model.enums.Role;
+import com.secomp.clinica.repository.ConsultaRepository;
+import com.secomp.clinica.repository.PacienteRepository;
+import com.secomp.clinica.repository.UsuarioRepository;
+import com.secomp.clinica.util.Horario;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
+import java.text.SimpleDateFormat;
+import java.time.LocalTime;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/medico")
+public class MedicoController {
+
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        dateFormat.setLenient(false);
+        binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, true));
+    }
+
+    private final UsuarioRepository usuarioRepository;
+    private final PacienteRepository pacienteRepository;
+
+    @Autowired
+    public MedicoController(UsuarioRepository usuarioRepository, PacienteRepository pacienteRepository) {
+        this.usuarioRepository = usuarioRepository;
+        this.pacienteRepository = pacienteRepository;
+    }
+
+    @GetMapping
+    public String medico() {
+        return "medico/index";
+    }
+
+    @GetMapping("/list")
+    public String listar(Model model) {
+        model.addAttribute("pacientes", pacienteRepository.findAll());
+        return "/paciente/list";
+    }
+
+}
