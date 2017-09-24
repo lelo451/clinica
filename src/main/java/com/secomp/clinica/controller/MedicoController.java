1210e5ae3088c921f1f18cb1ceaa18b33c6e871d
Vinicius <vinicamarggo@hotmail.com>
2017-09-24 04:39:23 -0300

    prontuario

--- src/main/java/com/secomp/clinica/controller/MedicoController.java
+++ src/main/java/com/secomp/clinica/controller/MedicoController.java
@@ -12,14 +12,15 @@ import com.secomp.clinica.util.Horario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -81,7 +82,7 @@ public class MedicoController {
         }
         System.out.println(prontuario);
         Paciente paciente = prontuario.getPaciente();
-        prontuario.setMedico(SecurityContextHolder.getContext().getAuthentication().getName());
+        //prontuario.setMedico(SecurityContextHolder.getContext().getAuthentication().getName());
         paciente.setProntuario(prontuario);
         pacienteRepository.save(paciente);
         ra.addFlashAttribute("sucesso", "Prontuário cadastrado com sucesso!");
