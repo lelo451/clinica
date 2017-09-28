c29295e782f75b3ccd3266d7fd0195f131fecd37
leo <ra98995@uem.br>
2017-09-28 10:11:11 -0300

    remove unnecessary fields

--- src/main/java/com/secomp/clinica/model/Paciente.java
+++ src/main/java/com/secomp/clinica/model/Paciente.java
@@ -5,7 +5,6 @@ import org.hibernate.validator.constraints.NotEmpty;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Null;
 import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.Date;
